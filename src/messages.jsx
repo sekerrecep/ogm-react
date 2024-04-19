cfd26e10aa89eef0de7d45edf79303d45e40a89f
recep seker <recepseker@ogm.gov.tr>
2024-04-19 12:12:49 +0300

    react-intl added

---
+++ src/messages.jsx
@@ -0,0 +1,13 @@
+const messages = {
+    "tr-TR": {
+        title: "Başlık",
+    },
+    "en-US": {
+        title: "Title",
+    },
+    "de-DE": {
+        title: "Titel",
+    },
+};
+
+export default messages;
